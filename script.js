0974486e21da9d98bac71e63a27d78556aa0c50b
Knight-4000 <yorksprinters@gmail.com>
2023-06-03 10:42:20 -0400

    Updated form validation.

--- script.js
+++ script.js
@@ -13,9 +13,43 @@ function showModal() {
     websiteNameEl.focus();
 }
 
-// Event Listener
+// Modal Event Listeners
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 // If click inside modal, nothing happens. If click outside, show-modal class 
 // is removed and the modal disappears. 
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
+
+// Validate Form
+
+function validate(nameValue, urlValue) {
+    const expression = /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;    const regex = new RegExp(expression);
+    // If no name value OR no url value 
+    if (!nameValue || !urlValue) {
+        alert('Try entering something in both fields');
+        return false;
+    }
+    if (!urlValue.match(regex)) {
+        alert('Please provide a valid web address');
+        return false;
+    }
+    return true;
+}
+
+// Handle Data from Form
+
+function storeBookmark(e) {
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
+        urlValue = `https://${urlValue}`; 
+   }
+  if (!validate(nameValue, urlValue)) {
+    return false;
+  }
+}
+
+// Form Event Listener
+
+bookMarkForm.addEventListener('submit', storeBookmark);
